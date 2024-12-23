e134a10496cd21878cf760389f8f7d91e26d935b
huna-dayo <nkhshc421026@g.neec.ac.jp>
2024-12-23 13:17:44 +0900

    kadai6-3

---
+++ 6-3/6-3-2.mjs
@@ -0,0 +1,4 @@
+const baseArray1 = [60,70,80,90]
+const baseArray2 = [10,20,30,40,50]
+console.log(baseArray1.every(value => value > 50))
+console.log(baseArray2.every(value => value > 50))
---
+++ 6-3/6-3-3.mjs
@@ -0,0 +1,4 @@
+const baseArray1 = [98, 85, 80]
+const baseArray2 = [-98, -79]
+console.log(baseArray1.some(value => value < 0))
+console.log(baseArray2.some(value => value < 0))
